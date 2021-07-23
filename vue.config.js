916834fc6cb71d8bdec48a38d1dc915c8faabbe0
« Nicolas <n_kloc@etu-webschoolfactory.fr>
2021-07-24 01:54:28 +0200

    add: vue.config

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = "Nicolas KLOC, front-end developer";
+                return args;
+            })
+    }
+}
