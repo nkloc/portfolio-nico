050769bb842d696b0088e2c104ecdc7a196226dc
« Nicolas <n_kloc@etu-webschoolfactory.fr>
2023-03-03 12:11:33 +0100

    changes title

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
         config
             .plugin('html')
             .tap(args => {
-                args[0].title = "Nicolas KLOC, front-end developer";
+                args[0].title = "Nicolas KLOC, Product Manager";
                 return args;
             })
     }
